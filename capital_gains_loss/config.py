e66f70fcd7ebd84b40cf2554bc9d427ea134633f
sarbjit87 <sarbjit1987@gmail.com>
2021-01-16 17:49:35 -0500

    Integrate Recaptcha

--- capital_gains_loss/config.py
+++ capital_gains_loss/config.py
@@ -8,14 +8,18 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
+    #MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.sendgrid.net'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
     CSV_UPLOAD = os.path.join(basedir, 'uploads')
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
     SESSION_REFRESH_EACH_REQUEST = True
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
 class TestConfig:
     SECRET_KEY = '13b8184b924cf6f8e31fdf3fdssdd3725' #secrets.token_hex(16)
@@ -31,3 +35,5 @@ class TestConfig:
     WTF_CSRF_ENABLED = False
     TESTING = True
     BCRYPT_LOG_ROUNDS = 4
+    RECAPTCHA_PUBLIC_KEY = "your_key"
+    RECAPTCHA_PRIVATE_KEY = "your_key"
--- capital_gains_loss/users/forms.py
+++ capital_gains_loss/users/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -15,6 +15,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    recaptcha = RecaptchaField()
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -33,7 +34,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
+    recaptcha = RecaptchaField()
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
