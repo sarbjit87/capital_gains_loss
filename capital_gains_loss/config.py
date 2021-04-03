68fb37890929361f4dd5cbb032167b79fff1a9f5
sarbjit <sarbjit1987@gmail.com>
2021-04-03 13:38:55 -0400

    Production DB

--- capital_gains_loss/config.py
+++ capital_gains_loss/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') #secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #MAIL_SERVER = 'smtp.googlemail.com'
